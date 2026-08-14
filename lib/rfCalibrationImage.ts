c30ed499940fc236acab998dbf985417db4a8ef6
aman-vishen <71383626+aman-vishen@users.noreply.github.com>
2026-08-14 18:20:08 +0530

    Assemble realistic RF calibration image

---
+++ lib/rfCalibrationImage.ts
@@ -0,0 +1,7 @@
+import chunk1 from './rfCalibrationImageChunk1';
+import chunk2 from './rfCalibrationImageChunk2';
+import chunk3 from './rfCalibrationImageChunk3';
+import chunk4 from './rfCalibrationImageChunk4';
+import chunk5 from './rfCalibrationImageChunk5';
+
+export const rfCalibrationImage = `data:image/webp;base64,${chunk1}${chunk2}${chunk3}${chunk4}${chunk5}`;
